0648c977792b21dc16a711f212490c2c6be13d58
spielhuus <spielhuus@gmail.com>
2018-03-05 10:07:12 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,34 @@
+## This repository holds a conan recipe for hiredis.
+
+[![Build Status](https://travis-ci.org/spielhuus/conan-hiredis.svg?branch=master)](https://travis-ci.org/spielhuus/conan-hiredis)
+[ ![Download](https://api.bintray.com/packages/squawkcpp/conan-cpp/Hiredis%3Aconan-cpp/images/download.svg) ](https://bintray.com/squawkcpp/conan-cpp/Hiredis%3Aconan-cpp)
+
+[Conan.io](https://conan.io) package for [hiredis](https://github.com/redis/hiredis) project
+
+The packages generated with this **conanfile** can be found in [Bintray](https://bintray.com/squawkcpp/conan-cpp/Hiredis%3Aconan-cpp).
+
+## Use this package
+
+### Basic setup
+
+    $ conan install Hiredis/0.13.3@conan-cpp/latest
+
+### Project setup
+
+If you handle multiple dependencies in your project is better to add a *conanfile.txt*
+
+    [requires]
+    Hiredis/0.13.3@conan-cpp/latest
+
+    [generators]
+    txt
+
+Complete the installation of requirements for your project running:
+
+    $ mkdir build && cd build && conan install ..
+
+Note: It is recommended that you run conan install from a build directory and not the root of the project directory.  This is because conan generates *conanbuildinfo* files specific to a single build configuration which by default comes from an autodetected default profile located in ~/.conan/profiles/default .  If you pass different build configuration options to conan install, it will generate different *conanbuildinfo* files.  Thus, they should not be added to the root of the project, nor committed to git.
+
+### License
+[Boost Software License](http://www.boost.org/LICENSE_1_0.txt) - Version 1.0 - August 17th, 2003
+
---
+++ conanfile.py
@@ -0,0 +1,48 @@
+from conans import ConanFile, tools
+from conans.tools import download, untargz
+import os
+
+class HiredisConan(ConanFile):
+    name = "Hiredis"
+    version = "0.13.3"
+    description = "Hiredis is a minimalistic C client library for the Redis database."
+    url = "https://github.com/redis/hiredis"
+    license = "BSD 3-clause \"New\" or \"Revised\" License"
+    FOLDER_NAME = 'hiredis-%s' % version
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False],
+               "disable_threads": [True, False]}
+    default_options = "shared=False", "disable_threads=False"
+
+    def run_bash(self, cmd):
+        if self.settings.os == "Windows":
+            tools.run_in_windows_bash(self, cmd)
+        else:
+            self.run(cmd)
+
+    def source(self):
+        tarball_name = self.FOLDER_NAME + '.tar.gz'
+        download("https://github.com/redis/hiredis/archive/v%s.tar.gz" % (self.version),
+                 tarball_name)
+        untargz(tarball_name)
+        os.unlink(tarball_name)
+
+    def build(self):
+        with tools.chdir("hiredis-%s" % self.version) :
+            self.run_bash("make")
+        
+    def package(self):
+        self.copy("*.h", dst="include/hiredis", src="%s" % (self.FOLDER_NAME))
+        self.copy("*.h", dst="include/hiredis/adapters", src="%s/adapters" % (self.FOLDER_NAME))
+        if self.options.shared:
+            if self.settings.os == "Macos":
+                self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+            else:
+                self.copy(pattern="*.so*", dst="lib", keep_path=False)
+        else:
+            self.copy(pattern="*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        if self.settings.os == "Linux" or self.settings.os == "Macos":
+            self.cpp_info.libs = ['hiredis']
+
